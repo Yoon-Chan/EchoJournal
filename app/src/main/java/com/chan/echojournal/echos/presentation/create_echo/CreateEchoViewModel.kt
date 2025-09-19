4d886ff677a1385afe61eda567379fcb0ab405eb
윤찬 <vsvx13@naver.com>
2025-09-19 12:37:53 +0900

    [feat]: Sheet 관련 액션 기능 구현

--- app/src/main/java/com/chan/echojournal/echos/presentation/create_echo/CreateEchoViewModel.kt
+++ app/src/main/java/com/chan/echojournal/echos/presentation/create_echo/CreateEchoViewModel.kt
@@ -1,15 +1,18 @@
 package com.chan.echojournal.echos.presentation.create_echo
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chan.echojournal.echos.presentation.models.MoodUI
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
 class CreateEchoViewModel constructor(
 
-): ViewModel() {
+) : ViewModel() {
 
     private var hasLoadedInitialData = false
     private val _state = MutableStateFlow(CreateEchoState())
@@ -29,11 +32,13 @@ class CreateEchoViewModel constructor(
         when (action) {
             is CreateEchoAction.OnAddTopicTextChange -> {}
             CreateEchoAction.OnCancelClick -> {}
-            CreateEchoAction.OnConfirmMood -> {}
+            CreateEchoAction.OnConfirmMood -> onConfirmMood()
             CreateEchoAction.OnCreateNewTopicClick -> {}
-            CreateEchoAction.OnDismissMoodSelector -> {}
+            CreateEchoAction.OnDismissMoodSelector -> onDismissMoodSelector()
+
             CreateEchoAction.OnDismissTopicSuggestions -> {}
-            is CreateEchoAction.OnMoodClick -> {}
+            is CreateEchoAction.OnMoodClick -> onClickMoodClick(action.moodUI)
+
             CreateEchoAction.OnNavigateBackClick -> {}
             is CreateEchoAction.OnNoteTextChange -> {}
             CreateEchoAction.OnPauseAudioClick -> {}
@@ -45,7 +50,37 @@ class CreateEchoViewModel constructor(
             is CreateEchoAction.OnTrackSizeAvailable -> {}
 
             CreateEchoAction.ShowMoodSelector -> {}
-            CreateEchoAction.OnSelectMoodClick ->{}
+            CreateEchoAction.OnSelectMoodClick -> onSelectMoodClick()
+        }
+    }
+
+    private fun onConfirmMood() {
+        _state.update {
+            it.copy(
+                mood = it.selectedMood,
+                canSaveEcho = it.titleText.isNotBlank(),
+                showMoodSelector = false
+            )
+        }
+    }
+
+    private fun onDismissMoodSelector() {
+        _state.update {
+            it.copy(
+                showMoodSelector = false
+            )
+        }
+    }
+
+    private fun onSelectMoodClick() {
+        _state.update {
+            it.copy(showMoodSelector = true)
+        }
+    }
+
+    private fun onClickMoodClick(moodUI: MoodUI) {
+        _state.update {
+            it.copy(selectedMood = moodUI)
         }
     }
 }
